b0fc35748494b4a1e425d3d036a45c602037e9f6
wifelette <leahsilber@gmail.com>
2015-10-04 10:50:33 -0700

    Removes headshot field validation

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -8,7 +8,6 @@ class Speaker < ActiveRecord::Base
   delegate :name, :email, :gravatar_hash, to: :person
 
   validates :bio, presence: true, length: { maximum: 500 }
-  validates :headshot_url, length: { minimum: 10}
   validates :location, :github, presence: true
 end
 
